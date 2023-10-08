a809e326516ff681b0f9a24fbc8e4e05c4debb29
Raunak <112802366+rk8287@users.noreply.github.com>
2023-10-08 16:06:49 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,4 +1,4 @@
-const ErrorHander = require("../utils/errorHander");
+const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("./catchAsyncErrors");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
