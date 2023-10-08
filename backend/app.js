78bdc1c6907a081b4ad65254960fd8bedbb53cc6
Raunak <112802366+rk8287@users.noreply.github.com>
2023-10-08 16:34:33 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,45 +1,35 @@
-const express = require('express')
-const app = express()
-const errMiddelWare = require('./middleware/error')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const fileUpload = require('express-fileupload')
+const express = require('express');
+const app = express();
+const errMiddleware = require('./middleware/error');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 const path = require("path");
+const dotenv = require('dotenv');
 
+// Load environment variables
+dotenv.config({ path: "backend/config/config.env" });
 
-// Config
-if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({ path: "backend/config/config.env" });
-}
-
-app.use(express.json({ limit: '10mb' }))
+app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(fileUpload());
 
-//Route imports
+// Route imports
+const product = require("./routes/productRoute");
+const user = require('./routes/userRoute');
+const order = require('./routes/orderRoute');
+const payment = require('./routes/paymentRoute');
 
-const product = require("./routes/productRoute")
-const user = require('./routes/userRoute')
-const order = require('./routes/orderRoute')
-const payment = require('./routes/paymentRoute')
+// Serve static files (including 'index.html') from the 'client/build' directory
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.use('/api/v1',product)
-app.use('/api/v1',user)
-app.use('/api/v1',order)
-app.use('/api/v1',payment)
-
-
-app.use(express.static(path.join(__dirname, "./client/build")));
-
+// Catch-all route to serve 'index.html' for frontend routing
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
+// Middleware for handling errors
+app.use(errMiddleware);
 
-//Middle ware for Error
-
-app.use(errMiddelWare)
-
-
-module.exports = app
+module.exports = app;
