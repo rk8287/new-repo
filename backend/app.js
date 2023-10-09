814ca8bb2ee98df1f47c5406e719379d823b7085
Raunak <112802366+rk8287@users.noreply.github.com>
2023-10-09 16:34:12 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -21,8 +21,10 @@ const user = require('./routes/userRoute');
 const order = require('./routes/orderRoute');
 const payment = require('./routes/paymentRoute');
 
-
-
+app.use('/api/v1',product)
+app.use('/api/v1',user)
+app.use('/api/v1',order)
+app.use('/api/v1',payment)
 
 
 // Middleware for handling errors
