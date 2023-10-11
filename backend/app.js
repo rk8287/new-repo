93fd9e1ad201246e2bf8f9ccfd32a9ba355949f9
Raunak <112802366+rk8287@users.noreply.github.com>
2023-10-11 14:38:30 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const path = require('path');
 const dotenv = require('dotenv');
+const cors = require('cors');
+
 
 // Load environment variables
 dotenv.config({ path: 'backend/config/config.env' });
@@ -26,6 +28,10 @@ app.use('/api/v1',user)
 app.use('/api/v1',order)
 app.use('/api/v1',payment)
 
+app.use(cors({
+  origin: 'https://your-netlify-app-url.netlify.app', // Replace with your Netlify app URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}));
 
 // Middleware for handling errors
 app.use(errMiddleware);
