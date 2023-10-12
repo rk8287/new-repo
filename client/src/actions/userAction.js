079684505f453e48a154f690170e7201a3d76957
Raunak <112802366+rk8287@users.noreply.github.com>
2023-10-12 09:59:05 +0530

    Update userAction.js

--- client/src/actions/userAction.js
+++ client/src/actions/userAction.js
@@ -1,11 +1,12 @@
-import {LOGIN_REQUEST,
+import {
+  LOGIN_REQUEST,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   CLEAR_ERROR,
   REGISTER_REQUEST,
   REGISTER_SUCCESS,
   REGISTER_FAIL,
-   LOAD_USER_REQUEST,
+  LOAD_USER_REQUEST,
   LOAD_USER_SUCCESS,
   LOAD_USER_FAIL,
   LOGOUT_SUCCESS,
@@ -28,30 +29,26 @@ import {LOGIN_REQUEST,
   USER_DETAILS_REQUEST,
   USER_DETAILS_SUCCESS,
   USER_DETAILS_FAIL,
-  
-} from '../constants/userConstant';
+} from "../constants/userConstant";
 
-import axios from 'axios'
+import axios from "axios";
 
-//Login Action
+const apiUrl = 'https://my-ecommerce-xwc5.onrender.com/api/v1';
 
+// Login Action
 export const login = (email, password) => async (dispatch) => {
-    try {
-      dispatch({ type: LOGIN_REQUEST });
-  
-      const config = { headers: { "Content-Type": "application/json" } };
-  
-      const { data } = await axios.post(
-        `/api/v1/login`,
-        { email, password },
-        config
-      );
-  
-      dispatch({ type: LOGIN_SUCCESS, payload: data.user });
-    } catch (error) {
-      dispatch({ type: LOGIN_FAIL, payload: error.response.data.message });
-    }
-  };
+  try {
+    dispatch({ type: LOGIN_REQUEST });
+
+    const config = { headers: { "Content-Type": "application/json" } };
+
+    const { data } = await axios.post(`${apiUrl}/login`, { email, password }, config); // Use apiUrl here
+
+    dispatch({ type: LOGIN_SUCCESS, payload: data.user });
+  } catch (error) {
+    dispatch({ type: LOGIN_FAIL, payload: error.response.data.message });
+  }
+};
 
 // Register
 export const register = (userData) => async (dispatch) => {
@@ -60,7 +57,7 @@ export const register = (userData) => async (dispatch) => {
 
     const config = { headers: { "Content-Type": "multipart/form-data" } };
 
-    const { data } = await axios.post(`/api/v1/register`, userData, config);
+    const { data } = await axios.post(`${apiUrl}/register`, userData, config); // Use apiUrl here
 
     dispatch({ type: REGISTER_SUCCESS, payload: data.user });
   } catch (error) {
@@ -71,25 +68,23 @@ export const register = (userData) => async (dispatch) => {
   }
 };
 
-//Load User
-
+// Load User
 export const loadUser = () => async (dispatch) => {
   try {
     dispatch({ type: LOAD_USER_REQUEST });
 
-    const { data } = await axios.get(`/api/v1/me`);
+    const { data } = await axios.get(`${apiUrl}/me`); // Use apiUrl here
 
     dispatch({ type: LOAD_USER_SUCCESS, payload: data.user });
   } catch (error) {
     dispatch({ type: LOAD_USER_FAIL, payload: error.response.data.message });
   }
 };
-  
 
 // Logout User
 export const logout = () => async (dispatch) => {
   try {
-    await axios.get(`/api/v1/logout`);
+    await axios.get(`${apiUrl}/logout`); // Use apiUrl here
 
     dispatch({ type: LOGOUT_SUCCESS });
   } catch (error) {
@@ -104,7 +99,7 @@ export const updateProfile = (userData) => async (dispatch) => {
 
     const config = { headers: { "Content-Type": "multipart/form-data" } };
 
-    const { data } = await axios.put(`/api/v1/me/update`, userData, config);
+    const { data } = await axios.put(`${apiUrl}/me/update`, userData, config); // Use apiUrl here
 
     dispatch({ type: UPDATE_PROFILE_SUCCESS, payload: data.success });
   } catch (error) {
@@ -115,7 +110,6 @@ export const updateProfile = (userData) => async (dispatch) => {
   }
 };
 
-
 // Update Password
 export const updatePassword = (passwords) => async (dispatch) => {
   try {
@@ -123,11 +117,7 @@ export const updatePassword = (passwords) => async (dispatch) => {
 
     const config = { headers: { "Content-Type": "application/json" } };
 
-    const { data } = await axios.put(
-      `/api/v1/password/update`,
-      passwords,
-      config
-    );
+    const { data } = await axios.put(`${apiUrl}/password/update`, passwords, config); // Use apiUrl here
 
     dispatch({ type: UPDATE_PASSWORD_SUCCESS, payload: data.success });
   } catch (error) {
@@ -138,11 +128,12 @@ export const updatePassword = (passwords) => async (dispatch) => {
   }
 };
 
-// get All Users
+// Get All Users
 export const getAllUsers = () => async (dispatch) => {
   try {
     dispatch({ type: ALL_USERS_REQUEST });
-    const { data } = await axios.get(`/api/v1/admin/users`);
+
+    const { data } = await axios.get(`${apiUrl}/admin/users`); // Use apiUrl here
 
     dispatch({ type: ALL_USERS_SUCCESS, payload: data.users });
   } catch (error) {
@@ -150,12 +141,12 @@ export const getAllUsers = () => async (dispatch) => {
   }
 };
 
-
-// get  User Details
+// Get User Details
 export const getUserDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: USER_DETAILS_REQUEST });
-    const { data } = await axios.get(`/api/v1/admin/user/${id}`);
+
+    const { data } = await axios.get(`${apiUrl}/admin/user/${id}`); // Use apiUrl here
 
     dispatch({ type: USER_DETAILS_SUCCESS, payload: data.user });
   } catch (error) {
@@ -170,11 +161,7 @@ export const updateUser = (id, userData) => async (dispatch) => {
 
     const config = { headers: { "Content-Type": "application/json" } };
 
-    const { data } = await axios.put(
-      `/api/v1/admin/user/${id}`,
-      userData,
-      config
-    );
+    const { data } = await axios.put(`${apiUrl}/admin/user/${id}`, userData, config); // Use apiUrl here
 
     dispatch({ type: UPDATE_USER_SUCCESS, payload: data.success });
   } catch (error) {
@@ -190,7 +177,7 @@ export const deleteUser = (id) => async (dispatch) => {
   try {
     dispatch({ type: DELETE_USER_REQUEST });
 
-    const { data } = await axios.delete(`/api/v1/admin/user/${id}`);
+    const { data } = await axios.delete(`${apiUrl}/admin/user/${id}`); // Use apiUrl here
 
     dispatch({ type: DELETE_USER_SUCCESS, payload: data });
   } catch (error) {
@@ -201,12 +188,9 @@ export const deleteUser = (id) => async (dispatch) => {
   }
 };
 
-
-  //Clear ERRORS
-
-  export const clearErrors = () => async (dispatch)=>{
-    dispatch({
-        type: CLEAR_ERROR
-    })
-  }
-
+// Clear ERRORS
+export const clearErrors = () => async (dispatch) => {
+  dispatch({
+    type: CLEAR_ERROR,
+  });
+};
