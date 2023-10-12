b8dce2d1832b54061478281d0016e3327f59c6ba
Raunak <112802366+rk8287@users.noreply.github.com>
2023-10-12 09:19:50 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,16 +6,22 @@ const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const path = require('path');
 const dotenv = require('dotenv');
-
-
+const cors = require('cors'); // Import the cors middleware
 
 // Load environment variables
 dotenv.config({ path: 'backend/config/config.env' });
 
 app.use(express.json({ limit: '10mb' }));
-app.use(cookieParser());
+app.use(cookieParser();
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(fileUpload());
+
+// CORS configuration
+app.use(cors({
+  origin: 'https://ecommerce-web-app-8qzg.onrender.com', // Replace with your frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Enable cookies and credentials
+}));
 
 // Route imports
 const product = require('./routes/productRoute');
@@ -23,10 +29,10 @@ const user = require('./routes/userRoute');
 const order = require('./routes/orderRoute');
 const payment = require('./routes/paymentRoute');
 
-app.use('/api/v1',product)
-app.use('/api/v1',user)
-app.use('/api/v1',order)
-app.use('/api/v1',payment)
+app.use('/api/v1', product);
+app.use('/api/v1', user);
+app.use('/api/v1', order);
+app.use('/api/v1', payment);
 
 // Middleware for handling errors
 app.use(errMiddleware);
